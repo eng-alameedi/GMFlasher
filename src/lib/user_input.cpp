c9c7a9b7f18891db8fa1ed30243611ae3e28be29
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2025-01-11 23:09:45 +0300

    change function name, and return type

--- src/lib/user_input.cpp
+++ src/lib/user_input.cpp
@@ -1,10 +1,10 @@
 #include "user_input.h"
 
-bool input_validate(int arg_count, char** arg_v) {
+bool arguments_validate(int arg_count, char** arg_v) {
   int operation{};
   const char* options = "p:m:b:c:F:";
   while ((operation = getopt(arg_count, arg_v, options))) {
     // code here
   }
-  return false;
+  return true;
 }
