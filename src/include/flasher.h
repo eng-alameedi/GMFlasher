f844d78407750baddabba7395135f2d729bc8af8
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2025-01-11 23:11:00 +0300

    add new header for all headers information

---
+++ src/include/flasher.h
@@ -0,0 +1,13 @@
+#ifndef _FLASHER_H_
+#define _FLASHER_H_
+
+#include "user_input.h"
+#include "gui_api.h"
+
+#include <getopt.h>
+#include <ncurses.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILER 1
+
+#endif   // _FLASHER_H_
